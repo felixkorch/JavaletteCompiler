b8b639f8b375c39ad4b2a3d23bc52884118f1847
felixkorch <korch.felix@gmail.com>
2022-03-26 20:23:20 +0100

    Add type-error class

--- src/ValidationError.h
+++ src/ValidationError.h
@@ -15,3 +15,17 @@ public:
     	return msg_.c_str();
     }
 };
+
+class TypeError : public std::exception {
+    std::string msg_;
+public:
+    TypeError(const std::string& msg)
+    {
+        msg_ = "Javalette Type error: \"" + msg + "\"";
+    }
+
+    const char * what () const throw ()
+    {
+        return msg_.c_str();
+    }
+};

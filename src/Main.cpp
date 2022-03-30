e0465d112408f6e5b89d4e05854fe2394cec1a7a
felixkorch <korch.felix@gmail.com>
2022-03-30 17:45:22 +0200

    Refactor typechecker

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,64 +1,44 @@
 #include "bnfc/ParserError.H"
 #include "bnfc/Parser.H"
-#include "Validator.h"
-#include "ValidationError.h"
+#include "TypeChecker.h"
+#include "TypeError.h"
 
-#include <cstdio>
-#include <cstring>
-#include <string>
 #include <iostream>
-
-void usage() {
-  printf("usage: Call with one of the following argument combinations:\n");
-  printf("\t--help\t\tDisplay this help message.\n");
-  printf("\t(no arguments)\tParse stdin verbosely.\n");
-  printf("\t(files)\t\tParse content of files verbosely.\n");
-  printf("\t-s (files)\tSilent mode. Parse content of files silently.\n");
-}
+#include <memory>
 
 int main(int argc, char ** argv)
 {
     FILE *input;
-    int quiet = 0;
-    char *filename = NULL;
+    char *filename = nullptr;
 
-    if (argc > 1) {
-        if (strcmp(argv[1], "-s") == 0) {
-        quiet = 1;
-        if (argc > 2) {
-            filename = argv[2];
-        } else {
-            input = stdin;
-        }
-        } else {
-        filename = argv[1];
-        }
-    }
+
+    filename = argv[1];
 
     if (filename) {
         input = fopen(filename, "r");
         if (!input) {
-        usage();
-        exit(1);
+            exit(1);
         }
     } else input = stdin;
 
-    Prog *parse_tree = NULL;
+    bnfc::Prog *parse_tree = nullptr;
     try {
-        parse_tree = pProg(input);
-    } catch( parse_error &e) {
+        parse_tree = bnfc::pProg(input);
+    } catch( bnfc::parse_error &e) {
         std::cerr << "Parse error on line " << e.getLine() << "\n";
     }
+
     if (parse_tree) {
-        Validator* st = new Validator();
         try {
-            st->validate(parse_tree);
-        }catch(TypeError &e) {
+            typechecker::run(parse_tree);
+        }catch(typechecker::TypeError &e) {
+            std::cerr << e.what() << "\n";
+            return 1;
+        }catch(std::exception &e) {
             std::cerr << e.what() << "\n";
             return 1;
         }
-        printf("Program is valid!\n");
-        delete st;
+        std::cout << "Program is valid!" << std::endl;
         delete parse_tree;
         return 0;
     }
---
+++ src/TypeError.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <exception>
+#include <cstring>
+#include <sstream>
+
+namespace typechecker {
+
+    class TypeError : public std::exception {
+        std::string msg_;
+    public:
+
+        TypeError(const std::string &msg, int lineNr, int charNr) {
+            std::stringstream ss;
+            ss << "error: " << lineNr << ":" << charNr << ": " << msg << std::endl;
+            msg_ = ss.str();
+        }
+
+        explicit TypeError(const std::string &msg) {
+            std::stringstream ss;
+            ss << "error: " << msg << std::endl;
+            msg_ = ss.str();
+        }
+
+        const char *what() const throw() {
+            return msg_.c_str();
+        }
+    };
+
+}

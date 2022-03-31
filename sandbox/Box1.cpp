43b03608c8a6c6cb1c314f8a2ebcc3d66f7dcd2a
Felix Korch <korch.felix@gmail.com>
2022-04-01 00:46:53 +0200

    Test print twice - works
    Means I can use printer as a common object

--- sandbox/Box1.cpp
+++ sandbox/Box1.cpp
@@ -1,4 +1,47 @@
-int main()
+#include "bnfc/ParserError.H"
+#include "bnfc/Parser.H"
+#include "src/TypeChecker.h"
+#include "src/TypeError.h"
+
+int main(int argc, char ** argv)
 {
+    FILE *input;
+    char *filename = nullptr;
+
+    filename = argv[1];
+
+    if (filename) {
+        input = fopen(filename, "r");
+        if (!input) {
+            exit(1);
+        }
+    } else input = stdin;
+
+    bnfc::Prog *parse_tree = nullptr;
+    try {
+        parse_tree = bnfc::pProg(input);
+    } catch( bnfc::parse_error &e) {
+        std::cerr << "ERROR: Parse error on line " << e.getLine() << "\n";
+    }
     
+    bnfc::PrintAbsyn printer;
+
+    if (parse_tree) {
+        try {
+            bnfc::Program* prog = (bnfc::Program*)parse_tree;
+            std::cout << printer.print(prog) << std::endl;
+            std::cout << printer.print(prog) << std::endl;
+            
+        }catch(typechecker::TypeError &e) {
+            std::cerr << e.what() << "\n";
+            return 1;
+        }catch(std::exception &e) {
+            std::cerr << e.what() << "\n";
+            return 1;
+        }
+        std::cerr << "OK" << std::endl;
+        delete parse_tree;
+        return 0;
+    }
+    return 1;
 }

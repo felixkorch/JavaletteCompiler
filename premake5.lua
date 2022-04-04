ddd094619cf38d508ea7bc6e80af0c545e701c44
Felix Korch <korch.felix@gmail.com>
2022-04-04 23:52:04 +0200

    Install tex file

--- premake5.lua
+++ premake5.lua
@@ -1,11 +1,11 @@
 function preExecuteLinux()
-    os.execute("bnfc -m -l -p bnfc --cpp -o bnfc src/Javalette.cf && cp -f src/Javalette.y bnfc/")
+    os.execute("bnfc -m -l -p bnfc --latex --cpp -o bnfc src/Javalette.cf && cp -f src/Javalette.y bnfc/")
     os.execute("flex -Pjavalette_ -o bnfc/Lexer.C bnfc/Javalette.l")
     os.execute("bison -t -pjavalette_ bnfc/Javalette.y -o bnfc/Parser.C")
 end
 
 function preExecuteWindows()
-    os.execute("bnfc.exe -m -l -p bnfc --cpp -o bnfc src/Javalette.cf")
+    os.execute("bnfc.exe -m -l -p bnfc --latex --cpp -o bnfc src/Javalette.cf")
     os.execute("xcopy /y src\\Javalette.y bnfc")
     os.execute(".\\flexbison\\win_flex.exe -Pjavalette_ -o bnfc/Lexer.C bnfc/Javalette.l")
     os.execute(".\\flexbison\\win_bison.exe -t -pjavalette_ bnfc/Javalette.y -o bnfc/Parser.C")
@@ -106,7 +106,7 @@ workspace "JavaletteCompiler"
 	   trigger     = "install",
 	   description = "Package the compiler source",
 	   execute = function ()
-            os.execute("mkdir -p install && cp Makefile.bk install/Makefile && cp -rf doc lib src install/")
+            os.execute("mkdir -p install && cp Makefile install/ && cp bnfc/Javalette.tex install/doc/ && cp -rf doc lib src install/")
 	        os.execute("cd install && tar -czf " .. install_name .. " doc lib src Makefile")
 	   end
 	}

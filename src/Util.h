d9c051e14ff5fdce0c504f80c920285b80f538cb
Felix Korch <korch.felix@gmail.com>
2022-04-09 16:26:49 +0200

    Fix bug & add readFile function

--- src/Util.h
+++ src/Util.h
@@ -2,24 +2,43 @@
 
 #include <optional>
 #include <vector>
+#include <unordered_map>
 
 namespace jlc {
 
 // Returns the (start, end) of a generic container, as a pair.
 // TODO: Does not work, would be cool to implement in the future
-template <class Container> constexpr auto range(Container c) {
+template <class Container> inline constexpr auto range(Container c) {
     return std::pair(std::begin(c), std::end(c));
 }
 
 // Returns the value& specified by the key as an optional
 // On success: opt<Val&>
 // On failure: opt<null>
+
+namespace map {
 template <class Key, class Val>
-auto findInMap(const Key& key, std::unordered_map<Key, Val>& map) {
+inline auto getValue(const Key& key, std::unordered_map<Key, Val>& map) {
     auto search = map.find(key);
     bool success = search != map.end();
     return success ? std::optional<std::reference_wrapper<Val>>{search->second}
                    : std::nullopt;
+}
+}
+
+// Tries to read the file with the given name. If fileName is empty,
+// start reading from std in. Exits(1) if failed, so guarantees to return a valid
+// file-pointer.
+inline FILE* readFileOrInput(const char* fileName) {
+    FILE* input;
+    if (fileName) {
+        input = fopen(fileName, "r");
+        if (!input) {
+            exit(1);
+        }
+    } else
+        input = stdin;
+    return input;
 }
 
 } // namespace jlc
